397b9a88cb8dd2a2a1dc995c4b7cf0ccbcd9bb9b
progsamdev <samuel.msbr@gmail.com>
2025-04-11 13:34:38 -0300

    add sleep

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func greet(phrase string) {
+	fmt.Println("Hello!", phrase)
+}
+
+func slowGreet(phrase string) {
+	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
+	fmt.Println("Hello!", phrase)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	greet("Nice to meet you!")
+	greet("How are you?")
+	slowGreet("How ... are ... you ...?")
+	greet("I hope you're liking the course!")
 }
